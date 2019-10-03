9f77417154acde6a85674fc9597c799a02dbe5fc
akim157 <akim157@mail.ru>
2019-10-03 16:05:43 +0300

    v1.1.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ItemList from './components/ItemList';
 
 ReactDOM.render(
-  <App />,
+  <ItemList />,
   document.getElementById('root')
 );
